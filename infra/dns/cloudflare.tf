9dd8338e10741443ee9c056e69feda9a3a62ac15
Bart Verhoeven <bart.verhoeven@amista.com>
2025-03-05 20:03:38 +0100

    FEATURE: www instead of root for homepage

--- infra/dns/cloudflare.tf
+++ infra/dns/cloudflare.tf
@@ -11,91 +11,82 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-resource "cloudflare_dns_record" "root1" {
+resource "cloudflare_dns_record" "www1" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "185.199.108.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "root2" {
+resource "cloudflare_dns_record" "www2" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "185.199.109.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "root3" {
+resource "cloudflare_dns_record" "www3" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "185.199.110.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "root4" {
+resource "cloudflare_dns_record" "www4" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "185.199.111.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "rootAAAA1" {
+resource "cloudflare_dns_record" "wwwAAAA1" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "2606:50c0:8000::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "rootAAAA2" {
+resource "cloudflare_dns_record" "wwwAAAA2" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "2606:50c0:8001::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "rootAAAA3" {
+resource "cloudflare_dns_record" "wwwAAAA3" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "2606:50c0:8002::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "rootAAAA4" {
+resource "www.cloudflare_dns_record" "wwwAAAA4" {
   zone_id = var.zone_id
-  name    = "bartoutofthebox.com"
+  name    = "www.bartoutofthebox.com"
   content = "2606:50c0:8003::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "www" {
+resource "cloudflare_dns_record" "root" {
   zone_id = var.zone_id
-  name    = "www"
-  content = "bartoutofthebox.com"
-  type    = "CNAME"
-  proxied = false
-  ttl     = 3600
-}
-
-resource "cloudflare_dns_record" "_domainconnect" {
-  zone_id = var.zone_id
-  name    = "_domainconnect"
-  content = "_domainconnect.gd.domaincontrol.com"
+  name    = "bartoutofthebox.com"
+  content = "www.bartoutofthebox.com"
   type    = "CNAME"
   proxied = false
   ttl     = 3600
