1af50dc8952713d64312b670151148cc7a3aea7a
Bart Verhoeven <bart.verhoeven@amista.com>
2025-03-05 21:10:55 +0100

    FIX

--- infra/dns/cloudflare.tf
+++ infra/dns/cloudflare.tf
@@ -11,82 +11,82 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-resource "cloudflare_dns_record" "www1" {
+resource "cloudflare_dns_record" "rootA1" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "185.199.108.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "www2" {
+resource "cloudflare_dns_record" "rootA2" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "185.199.109.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "www3" {
+resource "cloudflare_dns_record" "rootA3" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "185.199.110.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "www4" {
+resource "cloudflare_dns_record" "rootA4" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "185.199.111.153"
   type    = "A"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "wwwAAAA1" {
+resource "cloudflare_dns_record" "rootAAAA1" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "2606:50c0:8000::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "wwwAAAA2" {
+resource "cloudflare_dns_record" "rootAAAA2" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "2606:50c0:8001::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "wwwAAAA3" {
+resource "cloudflare_dns_record" "rootAAAA3" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "2606:50c0:8002::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "wwwAAAA4" {
+resource "cloudflare_dns_record" "rootAAAA4" {
   zone_id = var.zone_id
-  name    = "www.bartoutofthebox.com"
+  name    = "bartoutofthebox.com"
   content = "2606:50c0:8003::153"
   type    = "AAAA"
   proxied = false
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "root" {
+resource "cloudflare_dns_record" "www" {
   zone_id = var.zone_id
   name    = "bartoutofthebox.com"
-  content = "www.bartoutofthebox.com"
+  content = "bartoutofthebox.github.io"
   type    = "CNAME"
   proxied = false
   ttl     = 3600
@@ -116,7 +116,7 @@ resource "cloudflare_dns_record" "wildcard" {
 resource "cloudflare_dns_record" "github_verification" {
   zone_id = var.zone_id
   name    = "_github-pages-challenge-BartOutOfTheBox.bartoutofthebox.com"
-  content = "1591e97dd25716813d1d84eae6b2da"
+  content = "\"1591e97dd25716813d1d84eae6b2da\""
   type    = "TXT"
   proxied = false
   ttl     = 1
